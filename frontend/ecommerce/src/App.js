7f22d8722050e940af2a1fda472582eec8421130
Oshen1998 <oshenisuru33@gmail.com>
2020-11-05 09:56:04 +0530

    first comitt

---
+++ frontend/ecommerce/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainComponent from './Components/Maincomponent/main.component';
+
+function App() {
+  return (
+    <div>
+     <MainComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/ecommerce/src/Components/Maincomponent/main.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function MainComponent(){
+    return (
+       <h2 style={{textAlign:"center"}}>hello world!</h2>
+    );
+}
