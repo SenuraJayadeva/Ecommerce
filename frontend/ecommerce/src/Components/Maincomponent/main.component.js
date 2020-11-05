2fbfc8c3d742b5495749714bdbe91c2f9073a381
SenuraJayadeva <senurajayadeva@gmail.com>
2020-11-05 19:26:53 +0530

    frontend basic completed

--- frontend/ecommerce/src/Components/Maincomponent/main.component.js
+++ frontend/ecommerce/src/Components/Maincomponent/main.component.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React from "react";
+import ScriptTag from "react-script-tag";
+import $ from "jquery";
 
-export default function MainComponent(){
-    return (
-       <h2 style={{textAlign:"center"}}>hello world!</h2>
-    );
+import JS1 from "../js/jquery.slicknav";
+import JS2 from "../js/main";
+
+import HamburgerComponent from "../HamburgerComponent/hamburger.component";
+import Headersection from "../HeaderSectionComponent/headersection.component";
+import Herosection from "../HeroSectionComponent/herosection.component";
+import CategoriesSection from "../CategoriesSectionComponent/categoriessection.component";
+import FeaturedSection from "../FeaturedSectionComponent/featuredsection.component";
+
+export default function MainComponent() {
+  return (
+    <div>
+      <ScriptTag
+        type="text/javascript"
+        isHydrating={true}
+        src="../js/jquery-3.3.1.min.js"
+      />
+      <ScriptTag
+        type="text/javascript"
+        isHydrating={true}
+        src="../js/bootstrap.min.js"
+      />
+      <ScriptTag type="text/javascript" src="../js/jquery.nice-select.min.js" />
+      <ScriptTag
+        type="text/javascript"
+        isHydrating={true}
+        src="../js/jquery-ui.min.js"
+      />
+      <ScriptTag type="text/javascript" isHydrating={true} src={JS1} />
+      <ScriptTag
+        type="text/javascript"
+        isHydrating={true}
+        src="../js/mixitup.min.js"
+      />
+      <ScriptTag
+        type="text/javascript"
+        isHydrating={true}
+        src="../js/owl.carousel.min.js"
+      />
+      <ScriptTag type="text/javascript" isHydrating={true} src={JS2} />
+      <div>
+        <HamburgerComponent />
+      </div>
+
+      <div>
+        <Headersection />
+      </div>
+      <div>
+        <Herosection />
+      </div>
+      <div>
+        <CategoriesSection />
+      </div>
+      <div>
+        <FeaturedSection />
+      </div>
+    </div>
+  );
 }
