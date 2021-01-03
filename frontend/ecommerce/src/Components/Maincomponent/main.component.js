08ab63f899e5e139c50f8f16122987faeddd9795
SenuraJayadeva <senurajayadeva@gmail.com>
2021-01-03 23:13:43 +0530

    hamburger committ

--- frontend/ecommerce/src/Components/Maincomponent/main.component.js
+++ frontend/ecommerce/src/Components/Maincomponent/main.component.js
@@ -1,9 +1,4 @@
-import React from "react";
-import ScriptTag from "react-script-tag";
-import $ from "jquery";
-
-import JS1 from "../js/jquery.slicknav";
-import JS2 from "../js/main";
+import React, { useEffect } from "react";
 
 import HamburgerComponent from "../HamburgerComponent/hamburger.component";
 import Headersection from "../HeaderSectionComponent/headersection.component";
@@ -14,41 +9,11 @@ import FeaturedSection from "../FeaturedSectionComponent/featuredsection.compone
 export default function MainComponent() {
   return (
     <div>
-      <ScriptTag
-        type="text/javascript"
-        isHydrating={true}
-        src="../js/jquery-3.3.1.min.js"
-      />
-      <ScriptTag
-        type="text/javascript"
-        isHydrating={true}
-        src="../js/bootstrap.min.js"
-      />
-      <ScriptTag type="text/javascript" src="../js/jquery.nice-select.min.js" />
-      <ScriptTag
-        type="text/javascript"
-        isHydrating={true}
-        src="../js/jquery-ui.min.js"
-      />
-      <ScriptTag type="text/javascript" isHydrating={true} src={JS1} />
-      <ScriptTag
-        type="text/javascript"
-        isHydrating={true}
-        src="../js/mixitup.min.js"
-      />
-      <ScriptTag
-        type="text/javascript"
-        isHydrating={true}
-        src="../js/owl.carousel.min.js"
-      />
-      <ScriptTag type="text/javascript" isHydrating={true} src={JS2} />
       <div>
         <HamburgerComponent />
       </div>
 
-      <div>
-        <Headersection />
-      </div>
+      <div>{/* <Headersection /> */}</div>
       <div>
         <Herosection />
       </div>
