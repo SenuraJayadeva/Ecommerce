a346578ec1822adf6b821fa25937c0fe03cd49ad
Oshen1998 <oshenisuru33@gmail.com>
2021-01-06 00:05:12 +0530

    popup

--- frontend/ecommerce/src/Components/Maincomponent/main.component.js
+++ frontend/ecommerce/src/Components/Maincomponent/main.component.js
@@ -10,6 +10,7 @@ import ShoppinggridComponent from "../ShoppingGridComponent/shoppinggrid.compone
 import LoginComponent from "../LoginComponent/login.component";
 import RegisterComponent from "../RegisterComponent/register.component";
 import UserComponent from "../UserComponent/user.component";
+import loginPopup from "../popups/login.popup";
 
 export default function MainComponent() {
   return (
@@ -23,7 +24,7 @@ export default function MainComponent() {
           <Route path="/" exact component={HomePageComponent} />
         </div>
         <div>
-          <Route path="/login" exact component={LoginComponent} />
+          {/* <Route path="/login" exact component={loginPopup} /> */}
         </div>
         <div>
           <Route path="/signup" exact component={RegisterComponent} />
