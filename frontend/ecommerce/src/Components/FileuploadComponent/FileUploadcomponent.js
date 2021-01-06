b812c15515b255b3686945390aa8ed17bbed0296
SenuraJayadeva <senurajayadeva@gmail.com>
2021-01-06 22:29:42 +0530

    merge

--- frontend/ecommerce/src/Components/FileuploadComponent/FileUploadcomponent.js
+++ frontend/ecommerce/src/Components/FileuploadComponent/FileUploadcomponent.js
@@ -1,32 +1,34 @@
 import React from "react";
 import upload from "../FileuploadComponent/upload.css";
 
-import Dropzone from 'react-dropzone-uploader';
-import 'react-dropzone-uploader/dist/styles.css';
+import Dropzone from "react-dropzone-uploader";
+import "react-dropzone-uploader/dist/styles.css";
 
-export default function FileUpload(){
- // Payload data and url to upload files
- const getUploadParams = ({ meta }) => { return { url: 'https://http://localhost:3000' } }
+export default function FileUpload() {
+  // Payload data and url to upload files
+  const getUploadParams = ({ meta }) => {
+    return { url: "https://http://localhost:3000" };
+  };
 
- // Return the current status of files being uploaded
- const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta) }
+  // Return the current status of files being uploaded
+  const handleChangeStatus = ({ meta, file }, status) => {
+    console.log(status, meta);
+    console.log("image file " + meta.height);
+  };
 
- // Return array of uploaded files after submit button is clicked
- const handleSubmit = (files, allFiles) => {
-    //  console.log(files.map(f => f.meta))
-     allFiles.forEach(f => f.remove());
-     console.log("test");
-     console.log(files);
- }
+  // Return array of uploaded files after submit button is clicked
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map((f) => f.meta));
+    allFiles.forEach((f) => f.remove());
+  };
 
- return (
-     <Dropzone styles={{overflow:"hidden"}}
-     classNames={upload}
-         getUploadParams={getUploadParams}
-         onChangeStatus={handleChangeStatus}
-         maxFiles={3}
-        //  onSubmit={handleSubmit}
-         accept="image/*,audio/*,video/*"
-     />
- );
-};
+  return (
+    <Dropzone
+      getUploadParams={getUploadParams}
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+      accept="image/*,audio/*,video/*"
+    />
+  );
+}
