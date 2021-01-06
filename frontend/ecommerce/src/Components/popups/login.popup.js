525f4c09328bd5c2fe4d8dec5dfcf39675cbd833
Oshen1998 <oshenisuru33@gmail.com>
2021-01-06 23:11:59 +0530

    UI imrovements

--- frontend/ecommerce/src/Components/popups/login.popup.js
+++ frontend/ecommerce/src/Components/popups/login.popup.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginComponent from "../LoginComponent/login.component";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import "../popups/login.pop.css";
 
 export default function LoginPopup() {
-  return (
-    <Popup>
+ const [email , setEmail] = useState("");
+ const [password ,setPassword] = useState("");
+
+ const loginData = {
+   userEmail : email,
+   userPassword : password
+ }
+
+ const submitForm = (e)=>{
+   e.preventDefault();
+   console.log(loginData);
+ }
+
+  return (  
       <div className="container">
         <div className="container">
           <div className="row LoginRow">
             <div className="col-md-6 text-center loginComponentColOne">
-              <h1 className="textLoginColOne">UniCART</h1>
+             <h1 className="textLoginColOne d-none d-md-block">UniCART</h1>          
               <h3 className="textLoginColOne">LOGIN</h3>
               <hr style={{ color: "white" }} />
               <p className="textLoginColOne">Start posting your own ads</p>
@@ -21,11 +33,15 @@ export default function LoginPopup() {
             </div>
 
             <div className="col-md-6 loginComponentColTwo">
-              <form>
+              <form onSubmit={submitForm}>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Email address</label>
                   <input
                     type="email"
+                    value={email}
+                    onChange={(e)=>{
+                      setEmail(e.target.value);
+                    }}
                     class="form-control"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
@@ -36,6 +52,10 @@ export default function LoginPopup() {
                   <label for="exampleInputPassword1">Password</label>
                   <input
                     type="password"
+                    value={password}
+                    onChange={(e)=>{
+                      setPassword(e.target.value);
+                    }}
                     class="form-control"
                     id="exampleInputPassword1"
                     placeholder="Password"
@@ -52,6 +72,6 @@ export default function LoginPopup() {
           </div>
         </div>
       </div>
-    </Popup>
+
   );
 }
